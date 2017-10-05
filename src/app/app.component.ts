9c63350da002d6bfbad71d343692454a6fbe8c96
Jay Hongvarivatana <jayyhong@gmail.com>
2017-10-04 18:54:48 -0700

    Playing with scroll animations and sketch art

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AosToken } from './aos';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+  controller: any;
+
+  constructor( @Inject(AosToken) aos) {
+    aos.init();
+  }
+
+  ngOnInit() {
+
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': 0,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+    };
+    this.myParams = {
+      particles: {
+        number: {
+          value: 300,
+          desity: {
+            enable: true,
+            value_area: 500
+          }
+        },
+        color: {
+          value: '#EBA7BE'
+        },
+        shape: {
+          type: 'circle',
+        },
+        polygon: {
+          nb_sides: 5
+        },
+        opacity: {
+          value: 0.8,
+          random: false,
+          anim: {
+            enable: true,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 3,
+          random: true
+        },
+        line_linked: {
+          enable: false
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: {
+              enable: false
+            },
+          }
+        },
+        retina_detect: true
+      }
+    };
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AosToken, aos } from './aos';
 import { AppComponent } from './app.component';
+import { ParticlesModule } from 'angular-particle';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParticlesModule
   ],
-  providers: [],
+  providers: [
+    { provide: AosToken, useValue: aos }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
